a211a05e64cb6049d26a6a6a22cd13459f1bb81c
Demira11 <117664846+Demira11@users.noreply.github.com>
2023-04-02 12:53:24 -0400

    changed songs page

--- src/components/Songs.js
+++ src/components/Songs.js
@@ -1,9 +1,14 @@
 function Songs() {
   return (
-    <div>
-      This is Songs Page
+    <div className="songs">
+      <section>
+        <h2>Songs 2023</h2>
+      </section>
+      <section>
+        <h2>Songs 2022</h2>
+      </section>
     </div>
-  )
+  );
 }
 
 export default Songs;
