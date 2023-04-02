b69bc5855d1bae3b42011f58e9a6f8cdf0d8a3f2
Demira11 <117664846+Demira11@users.noreply.github.com>
2023-04-02 12:57:59 -0400

    added paragraph

--- src/components/Songs.js
+++ src/components/Songs.js
@@ -3,6 +3,7 @@ function Songs() {
     <div className="songs">
       <section>
         <h2>Songs 2023</h2>
+        <p>Most popular songs of 2023</p>
       </section>
       <section>
         <h2>Songs 2022</h2>
