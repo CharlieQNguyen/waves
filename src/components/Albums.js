4c3e9708d33605034556b027939b1180b6f06461
Albina Asllanaj <112036955+albinaasllanaj@users.noreply.github.com>
2023-04-04 12:29:23 -0400

    added containers

--- src/components/Albums.js
+++ src/components/Albums.js
@@ -2,7 +2,20 @@ import React from "react";
 
 const Albums = () => {
   return(
-    <div>Albums</div>
+    <div className="container">
+      <div className="topAlbums">
+        <h3>Top Albums of 2023</h3> 
+        </div>
+        <div className="hiphopAlbums">
+        <h3>Top Hip-Hop Albums of 2023</h3> 
+        </div>
+        <div className="popAlbums">
+        <h3>Top Pop Albums of 2023</h3> 
+        </div>
+        <div className="rnbAlbums">
+        <h3>Top RnB Albums of 2023</h3> 
+        </div>
+    </div>
   )
 }
 export default Albums;
