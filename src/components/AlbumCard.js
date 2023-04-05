d054ddaa2bd947132315cf4227a40fbd4292ea78
Albina Asllanaj <112036955+albinaasllanaj@users.noreply.github.com>
2023-04-05 15:15:39 -0400

    styled the card

--- src/components/AlbumCard.js
+++ src/components/AlbumCard.js
@@ -1,17 +1,21 @@
 function AlbumCard() {
-    return (
-      <div>
-        <div class="card">
-  <img src="..." className="card-img-top" alt="..."/>
+    return ( 
+        
+
+<div className="container">  
+<div className="card cardSection justfy-content-center">
+  <img src="..." className="img-fluid card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title">Card title</h5>
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     <button className="btn btn-primary">Go somewhere</button>
   </div>
 </div>
-      </div>
+</div>
+     
     );
   }
   
   export default AlbumCard;
   
+
