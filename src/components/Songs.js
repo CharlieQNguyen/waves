7cdcdc37de05bad7c43149cac15624594ea49afe
Demira11 <117664846+Demira11@users.noreply.github.com>
2023-04-05 13:14:35 -0400

    Builts the layout of page

--- src/components/Songs.js
+++ src/components/Songs.js
@@ -1,23 +1,47 @@
-import SongsCard from "./SongsCard";
+import React from "react";
 
-function Songs() {
+function PopSongsPage() {
   return (
-    <div className="songs">
-      <section>
-        <h2
-          className="text-center
-        "
-        >
-          Songs 2023
-          <SongsCard />
-        </h2>
-        <p>Most popular songs of 2023</p>
-      </section>
-      <section>
-        <h2>Songs 2022</h2>
-      </section>
+    <div>
+      <h1>Top Songs</h1>
+      <div className="song-list">
+        <div className="song-card">
+          <img
+            src="https://www.musicmundial.com/en/wp-content/uploads/2022/03/Dua-Lipa-is-sued-for-alleged-plagiarism-Levitating.jpg"
+            alt="Dua Lipa - Levitating"
+          />
+          <h2>Dua Lipa - Levitating</h2>
+          <p>
+            Levitating is a song by English singer Dua Lipa from her second
+            studio album, Future Nostalgia (2020).
+            <button>Listen</button>
+          </p>
+        </div>
+        <div className="song-card">
+          <img
+            src="https://i.scdn.co/image/ab67616d0000b2738863bc11d2aa12b54f5aeb36"
+            alt="The Weeknd - Blinding Lights"
+          />
+          <h2>The Weeknd - Blinding Lights</h2>
+          <p>
+            Blinding Lights is a song by Canadian singer The Weeknd, released as
+            the lead single from his fourth studio album, After Hours (2020).
+          </p>
+        </div>
+        <div className="song-card">
+          <img
+            src="https://pyxis.nymag.com/v1/imgs/848/851/4305fb42b7d9f5533ac9b8cb054c42f500-ariana-grande-album-review.rsquare.w700.jpg"
+            alt="Ariana Grande - positions"
+          />
+          <h2>Ariana Grande - positions</h2>
+          <p>
+            Positions is a song by American singer Ariana Grande, released as
+            the lead single from her sixth studio album of the same name (2020).
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default Songs;
+export default PopSongsPage;
--- src/components/SongsCard.js
+++ src/components/SongsCard.js
@@ -2,14 +2,9 @@ function SongsCard() {
   return (
     <div>
       <div className="card">
-        <img
-          src="https://variety.com/wp-content/uploads/2022/07/Music-Streaming-Wars.jpg?w=681&h=383&crop=1"
-          className="card-img-top;"
-          alt="..."
-        />
         <div className="card-body">
-          <h3>Rihanna</h3>
-          <p className="card-text">Don't Stop the Music</p>
+          <h3></h3>
+          <p className="card-text"></p>
         </div>
       </div>
     </div>
