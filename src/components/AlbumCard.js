ecc1da13ccdd2a538c4b65344c74930fc4602506
Albina Asllanaj <112036955+albinaasllanaj@users.noreply.github.com>
2023-04-05 14:43:27 -0400

    exported the AlbumCard

--- src/components/AlbumCard.js
+++ src/components/AlbumCard.js
@@ -1,12 +1,17 @@
-<div class="card" style="width: 18rem;">
+function AlbumCard() {
+    return (
+      <div>
+        <div class="card">
   <img src="..." className="card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title">Card title</h5>
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
+    <button className="btn btn-primary">Go somewhere</button>
   </div>
 </div>
-
-export default function(){
-    
-}
+      </div>
+    );
+  }
+  
+  export default AlbumCard;
+  
