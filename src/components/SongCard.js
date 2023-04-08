3f1159ac875b216d3cf8060065f3cc4c61e49e81
Demira11 <117664846+Demira11@users.noreply.github.com>
2023-04-08 13:33:35 -0400

    Added cards

---
+++ src/components/SongCard.js
@@ -0,0 +1,19 @@
+function SongsCard() {
+  return (
+    <div className="container">
+      <div className="card cardSection justfy-content-center">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/en/6/6a/UltraviolenceLDR.png"
+          className="img-fluid card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">Lana Del Rey</h5>
+          <p className="card-text">Ultraviolence</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SongsCard;
--- src/components/Songs.js
+++ src/components/Songs.js
@@ -1,48 +1,56 @@
 import React from "react";
 import "./Styles.css";
+import SongCard from "./SongCard";
 
-function PopSongsPage() {
+const Songs = () => {
   return (
-    <div>
-      <h1>Top Songs</h1>
-      <div className="song-list">
-        <div className="song-card">
-          <img
-            src="https://www.musicmundial.com/en/wp-content/uploads/2022/03/Dua-Lipa-is-sued-for-alleged-plagiarism-Levitating.jpg"
-            alt="Dua Lipa - Levitating"
-          />
-          <h2>Dua Lipa - Levitating</h2>
-          <p>
-            Levitating is a song by English singer Dua Lipa from her second
-            studio album, Future Nostalgia (2020).
-            <button>Listen</button>
-          </p>
+    <div className="container">
+      <div className="topSongs title">
+        <h3>Today's Top Songs</h3>
+
+        <div className="row">
+          <div className="col-3">
+            <SongCard />
+          </div>
+          <div className="col-3">
+            <SongCard />
+          </div>
+          <div className="col-3">
+            <SongCard />
+          </div>
+          <div className="col-3">
+            <SongCard />
+          </div>
         </div>
-        <div className="song-card">
-          <img
-            src="https://i.scdn.co/image/ab67616d0000b2738863bc11d2aa12b54f5aeb36"
-            alt="The Weeknd - Blinding Lights"
-          />
-          <h2>The Weeknd - Blinding Lights</h2>
-          <p>
-            Blinding Lights is a song by Canadian singer The Weeknd, released as
-            the lead single from his fourth studio album, After Hours (2020).
-          </p>
+      </div>
+      <div className="hiphopSongs title">
+        <h3>Hip-Hop</h3>
+        <div className="row">
+          <div className="col-3">
+            <SongCard />
+          </div>
+          <div className="col-3">
+            <SongCard />
+          </div>
+          <div className="col-3">
+            <SongCard />
+          </div>
+          <div className="col-3">
+            <SongCard />
+          </div>
         </div>
-        <div className="song-card">
-          <img
-            src="https://pyxis.nymag.com/v1/imgs/848/851/4305fb42b7d9f5533ac9b8cb054c42f500-ariana-grande-album-review.rsquare.w700.jpg"
-            alt="Ariana Grande - positions"
-          />
-          <h2>Ariana Grande - positions</h2>
-          <p>
-            Positions is a song by American singer Ariana Grande, released as
-            the lead single from her sixth studio album of the same name (2020).
-          </p>
-        </div>
+      </div>
+      <div className="countrySongs title">
+        <h3>Country</h3>
+      </div>
+
+      <div className="rnbSongs title">
+        <h3>Rhythm and Blues </h3>
+      </div>
+      <div className="popSongs title">
+        <h3>Pop</h3>
       </div>
     </div>
   );
-}
-
-export default PopSongsPage;
+};
+export default Songs;
