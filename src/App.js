cae031a2be23aad0f3da2b9a7fb3bf02d83137d4
Albina Asllanaj <112036955+albinaasllanaj@users.noreply.github.com>
2023-04-19 15:15:55 -0400

    added the clientID from spotify on the app.jk

--- src/App.js
+++ src/App.js
@@ -7,8 +7,18 @@ import Home from "./components/Home";
 import Songs from "./components/Songs";
 
 function App() {
+  
+const CLIENT_ID = "0283f12116d640dc8c380e85e158b619"
+const REDIRECT_URI = "http://localhost:3000"
+const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+const RESPONSE_TYPE = "token"
+
+
   return (
     <Router>
+
+<a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
+
       <Routes>
         <Route exact path="/Albums" element={<Albums />} />
         <Route exact path="/Artists" element={<Artists />} />
